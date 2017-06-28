a04e4ef0fd1d5594b99cbfc69712b452ea687b5d
barid92@gmail.com <barid92@gmail.com>
2017-06-28 13:38:12 +0200

    Podłączenie formularzu do komponentu

--- FormularzAngular4/src/app/app.component.ts
+++ FormularzAngular4/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  myForm: FormGroup;
+  post:any;  
+   constructor(private fb: FormBuilder) { 
+    this.myForm = fb.group({
+      'imie' : [null, Validators.required],
+      'nazwisko' : [null, Validators.compose([Validators.required, Validators.minLength(5)])],
+      'godzina' : [null, Validators.compose([Validators.required, Validators.pattern("([0-1]{1}[0-9]{1}|20|21|22|23):[0-5]{1}[0-9]{1}")])]
+    });
+  }
 }
